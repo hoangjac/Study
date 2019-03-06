5f38d1198663576cd7dea5fca1f44011edf96996
hoangjac <hoangjac@sheridancollege.ca>
2019-03-06 02:50:29 -0500

    parttime

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentTester.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentTester.java
@@ -11,7 +11,6 @@ public class StudentTester {
     public static void main(String[] args) {
         
         Student [] stuList = new Student[5];
-        stuList[0].setName("Lizzy");
         
         stuList[0]= new Student("Stu1",121);
         stuList[1]= new Student("Stu2",122);
@@ -23,6 +22,18 @@ public class StudentTester {
         for (int i=0;i<stuList.length;i++)
         {
             System.out.println ("Name:" + stuList[i].getName() + " ID: " + stuList[i].getID());
+        }
+        
+        Student [] partTime = new Student[5];
+        partTime[0]= new Student("Stu1",121);
+        partTime[1]= new Student("Stu2",122);
+        partTime[2]= new Student("Stu3",123);
+        partTime[3]= new Student("Stu4",124);
+        partTime[4]= new Student("Stu5",125);
+        
+        for (int i=0;i<partTime.length;i++)
+        {
+            System.out.println ("Name:" + partTime[i].getName() + " ID: " + partTime[i].getID());
         }
         
     }
