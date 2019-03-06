377572319885bfc0414607346aa8672845c4951e
hoangjac <44513509+hoangjac@users.noreply.github.com>
2019-03-06 02:33:42 -0500

    Update StudentTester.java

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentTester.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentTester.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+March 6th, 2019 
 package ca.sheridancollege.week1.softwarefundamentals;
 
 /**
